6ad48090580f1cf15bb3c67e3d0a15e003def7b7
blacktoast <dbstjd100@naver.com>
2025-07-29 16:06:10 +0900

    [feat] add timeout in showModal

--- sdk/ewallet_sdk_core/src/api/show_modal.ts
+++ sdk/ewallet_sdk_core/src/api/show_modal.ts
@@ -1,11 +1,27 @@
 import { KeplrEWallet } from "@keplr-ewallet-sdk-core/keplr_ewallet";
 import type { EWalletMsg } from "@keplr-ewallet-sdk-core/types";
 
+// 5 minutes
+const WAIT_TIME = 300000;
+
 export async function showModal(this: KeplrEWallet, msg: EWalletMsg) {
+  const timeout = new Promise<never>((_, reject) => {
+    setTimeout(() => reject(new Error("Show modal timeout")), WAIT_TIME);
+  });
+
   this.iframe.style.display = "block";
 
-  const showModalAck = await this.sendMsgToIframe(msg);
-  console.log("[showModal] [showModalAck] @@@@@", showModalAck);
+  let showModalAck: EWalletMsg;
+
+  try {
+    showModalAck = await Promise.race([this.sendMsgToIframe(msg), timeout]);
+  } catch (error) {
+    if (error instanceof Error && error.message === "Show modal timeout") {
+      await this.hideModal();
+      throw new Error("Show modal timeout");
+    }
+    throw error;
+  }
 
   return showModalAck;
 }
