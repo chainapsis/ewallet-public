21c0e0cf8b5af0dc773d67edaf6719c5f97755e7
devmosis <ryzond6728@gmail.com>
2025-07-31 15:16:58 +0900

    ewallet_sdk_core: fix type error

--- sdk/ewallet_sdk_core/src/api/sign_out.ts
+++ sdk/ewallet_sdk_core/src/api/sign_out.ts
@@ -3,6 +3,6 @@ import type { KeplrEWallet } from "@keplr-ewallet-sdk-core/keplr_ewallet";
 export async function signOut(this: KeplrEWallet) {
   await this.sendMsgToIframe({
     msg_type: "sign_out",
-    payload: {},
+    payload: null,
   });
 }
