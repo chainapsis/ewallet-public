14ff281eb29b4b186b9d017c1b206e3d2106f92e
chihunmanse <chihunmanse@gmail.com>
2025-08-01 13:05:28 +0900

    credential_vault: move interface to cv_interface

---
+++ credential_vault/cv_interface/src/auth.ts
@@ -0,0 +1,18 @@
+export interface GoogleTokenInfo {
+  alg: string;
+  at_hash: string;
+  aud: string;
+  azp: string;
+  email: string;
+  email_verified: string;
+  exp: string;
+  family_name: string;
+  given_name: string;
+  iat: string;
+  iss: string;
+  kid: string;
+  name: string;
+  picture: string;
+  sub: string;
+  typ: string;
+}
--- credential_vault/server/src/auth/google.ts
+++ credential_vault/server/src/auth/google.ts
@@ -1,31 +1,13 @@
 import type { Result } from "@keplr-ewallet/stdlib-js";
+import type { GoogleTokenInfo } from "@keplr-ewallet/credential-vault-interface";
 
 // TODO: This may change later
 const GOOGLE_CLIENT_ID =
   "239646646986-8on7ql1vmbcshbjk12bdtopmto99iipm.apps.googleusercontent.com";
 
-export interface GoogleTokenInfoResponse {
-  alg: string;
-  at_hash: string;
-  aud: string;
-  azp: string;
-  email: string;
-  email_verified: string;
-  exp: string;
-  family_name: string;
-  given_name: string;
-  iat: string;
-  iss: string;
-  kid: string;
-  name: string;
-  picture: string;
-  sub: string;
-  typ: string;
-}
-
 export async function validateOAuthToken(
   idToken: string,
-): Promise<Result<GoogleTokenInfoResponse, string>> {
+): Promise<Result<GoogleTokenInfo, string>> {
   try {
     const res = await fetch(
       `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${idToken}`,
@@ -33,7 +15,7 @@ export async function validateOAuthToken(
     if (!res.ok) {
       return { success: false, err: "Invalid token" };
     }
-    const tokenInfo = (await res.json()) as GoogleTokenInfoResponse;
+    const tokenInfo = (await res.json()) as GoogleTokenInfo;
 
     if (tokenInfo.aud !== GOOGLE_CLIENT_ID) {
       return { success: false, err: "Invalid client_id" };
