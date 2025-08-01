f15656234cd526fc107119a15891f5ed0379ad08
Elden Park <eldeniyenden@gmail.com>
2025-08-01 14:07:40 +0900

    o

--- sdk/ewallet_sdk_core/src/init/iframe.ts
+++ sdk/ewallet_sdk_core/src/init/iframe.ts
@@ -32,10 +32,10 @@ export function setupIframeElement(
   iframe.style.overflow = "hidden";
   iframe.style.zIndex = "1000000";
 
-  iframe.setAttribute(
-    "sandbox",
-    "allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox",
-  );
+  // iframe.setAttribute(
+  //   "sandbox",
+  //   "allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox",
+  // );
 
   // attach
   bodyEl.appendChild(iframe);
