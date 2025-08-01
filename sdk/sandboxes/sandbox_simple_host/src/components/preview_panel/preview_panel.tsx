cee76f56691d5ca54b151747a1b69a2d2b22c685
blacktoast <dbstjd100@naver.com>
2025-08-01 15:17:19 +0900

    [feat] apply tanstack query

--- sdk/sandboxes/sandbox_simple_host/src/components/preview_panel/preview_panel.tsx
+++ sdk/sandboxes/sandbox_simple_host/src/components/preview_panel/preview_panel.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import cn from "classnames";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import styles from "./preview_panel.module.scss";
 import { LoginWidget } from "@/components/widgets/login_widget/login_widget";
@@ -9,46 +11,55 @@ import { CosmosOnchainSignWidget } from "@/components/widgets/cosmos_onchain_sig
 import { CosmosOffChainSignWidget } from "@/components/widgets/cosmos_offchain_sign_widget/cosmos_offchain_sign_widget";
 import { EthereumOnchainSignWidget } from "@/components/widgets/ethereum_onchain_sign_widget/ethereum_onchain_sign_widget";
 import { CosmosOnchainCosmJsSignWidget } from "@/components/widgets/cosmos_onchain_cosmjs_sign_widget/cosmos_onchain_cosmjs_sign_widget";
-import { useKeplrEwallet } from "@/components/keplr_ewallet_provider/use_keplr_ewallet";
 
 export const PreviewPanel = () => {
-  const { cosmosEWallet, ethEWallet } = useKeplrEwallet();
-
-  console.log("cosmosEWallet !!!", cosmosEWallet);
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 1,
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  );
 
   return (
-    <div className={styles.wrapper}>
-      <div className={cn(styles.inner, "common-list-scroll")}>
-        <div className={styles.col}>
-          <LoginWidget />
-          {/* <AddressWidget /> */}
-          {/* <UserDataWidget userData={mockUserData} /> */}
-        </div>
-        <div className={styles.col}>
-          <h2>Ethereum</h2>
-          <EthereumOffchainSignWidget />
-          <EthereumOnchainSignWidget />
-        </div>
-        {/*   <SignWidget */}
-        {/*     chain="Ethereum" */}
-        {/*     chainIcon={<EthereumIcon />} */}
-        {/*     signType="onchain" */}
-        {/*     signButtonOnClick={() => {}} */}
-        {/*   /> */}
-        {/*   <DocsWidget /> */}
-        {/* </div> */}
+    <QueryClientProvider client={queryClient}>
+      <div className={styles.wrapper}>
+        <div className={cn(styles.inner, "common-list-scroll")}>
+          <div className={styles.col}>
+            <LoginWidget />
+            {/* <AddressWidget /> */}
+            {/* <UserDataWidget userData={mockUserData} /> */}
+          </div>
+          <div className={styles.col}>
+            <h2>Ethereum</h2>
+            <EthereumOffchainSignWidget />
+            <EthereumOnchainSignWidget />
+          </div>
+          {/*   <SignWidget */}
+          {/*     chain="Ethereum" */}
+          {/*     chainIcon={<EthereumIcon />} */}
+          {/*     signType="onchain" */}
+          {/*     signButtonOnClick={() => {}} */}
+          {/*   /> */}
+          {/*   <DocsWidget /> */}
+          {/* </div> */}
 
-        <div className={styles.col}>
-          <h2>Cosmos</h2>
-          <CosmosOffChainSignWidget />
-          <CosmosOnchainSignWidget />
-        </div>
-        <div className={styles.col}>
-          <h2>Cosmos (cosmjs)</h2>
-          <CosmosOnchainCosmJsSignWidget />
+          <div className={styles.col}>
+            <h2>Cosmos</h2>
+            <CosmosOffChainSignWidget />
+            <CosmosOnchainSignWidget />
+          </div>
+          <div className={styles.col}>
+            <h2>Cosmos (cosmjs)</h2>
+            <CosmosOnchainCosmJsSignWidget />
+          </div>
         </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 };
 
