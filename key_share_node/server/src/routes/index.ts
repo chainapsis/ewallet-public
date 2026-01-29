3f28cc6b90700d727910b0cb14322f80884081e6
chihunmanse <chihunmanse@gmail.com>
2026-01-29 16:11:09 +0900

    o

--- key_share_node/server/src/routes/index.ts
+++ key_share_node/server/src/routes/index.ts
@@ -4,7 +4,6 @@ import { makePgDumpRouter } from "./pg_dump";
 import { addStatusRoutes } from "./status";
 import { makeKeyshareRouter } from "./key_share/v1";
 import { makeKeyshareV2Router } from "./key_share_v2";
-import { makeCommitRevealRouter } from "./commit_reveal";
 
 export function setRoutes(app: Express) {
   const keyshareRouter = makeKeyshareRouter();
@@ -15,9 +14,6 @@ export function setRoutes(app: Express) {
 
   const keyshareV2Router = makeKeyshareV2Router();
   app.use("/keyshare/v2", keyshareV2Router);
-
-  const commitRevealRouter = makeCommitRevealRouter();
-  app.use("/commit-reveal/v2", commitRevealRouter);
 
   addStatusRoutes(app);
 }
