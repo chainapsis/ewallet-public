5670c754b040c350974e217e759510ee076f9443
devmosis <ryzond6728@gmail.com>
2026-01-30 10:21:18 +0900

    user_dashboard: update chain list and link to address pages

--- apps/user_dashboard/src/app/transaction_history/constant.ts
+++ apps/user_dashboard/src/app/transaction_history/constant.ts
@@ -3,30 +3,30 @@ export const TRANSACTION_HISTORY_SUPPORT_LIST = [
     chainId: "cosmoshub-4",
     explorerName: "Mintscan",
     explorerUrl: "https://www.mintscan.io/cosmos",
-  },
-  {
-    chainId: "osmosis-1",
-    explorerName: "Mintscan",
-    explorerUrl: "https://www.mintscan.io/osmosis",
+    addressPath: "/address",
   },
   {
     chainId: "eip155:1",
     explorerName: "Etherscan",
     explorerUrl: "https://etherscan.io",
+    addressPath: "/address",
   },
   {
-    chainId: "neutron-1",
-    explorerName: "Mintscan",
-    explorerUrl: "https://www.mintscan.io/neutron",
-  },
-  {
-    chainId: "omniflixhub-1",
-    explorerName: "EzStaking",
-    explorerUrl: "https://ezstaking.app/omniflixhub",
+    chainId: "solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp",
+    explorerName: "Solscan",
+    explorerUrl: "https://solscan.io",
+    addressPath: "/account",
   },
   {
     chainId: "interwoven-1",
     explorerName: "Initia Scan",
     explorerUrl: "https://scan.initia.xyz/interwoven-1",
+    addressPath: "/address",
+  },
+  {
+    chainId: "osmosis-1",
+    explorerName: "Mintscan",
+    explorerUrl: "https://www.mintscan.io/osmosis",
+    addressPath: "/address",
   },
 ];
