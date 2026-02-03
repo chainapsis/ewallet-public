c0c306063949066e8793b2cbb755f6225dd13082
Elden Park <eldeniyenden@gmail.com>
2026-02-02 16:38:50 -0800

    o

--- internals/ci/src/cmds/build_pkgs.ts
+++ internals/ci/src/cmds/build_pkgs.ts
@@ -1,48 +1,17 @@
-import { spawnSync } from "node:child_process";
-import chalk from "chalk";
-
-import { expectSuccess } from "@oko-wallet-ci/expect";
 import { paths } from "@oko-wallet-ci/paths";
-import { getPkgName } from "@oko-wallet-ci/pkg_name";
+import { buildInStages } from "@oko-wallet-ci/build";
 
 export async function buildPkgs(..._args: any[]) {
   await doBuildPkgs();
 }
 
 export async function doBuildPkgs() {
-  // Order matters!
-  const pkgsInOrder = [
-    paths.stdlib,
-    paths.dotenv,
-    paths.crypto_bytes,
-    paths.crypto_js,
-    paths.oko_types,
-    paths.teddsa_interface,
-    paths.ksn_interface,
-    paths.tecdsa_interface,
-    paths.github,
+  const stages = [
+    [paths.stdlib, paths.tecdsa_interface, paths.github],
+    [paths.dotenv, paths.crypto_bytes],
+    [paths.crypto_js, paths.oko_types],
+    [paths.teddsa_interface, paths.ksn_interface],
   ];
 
-  console.log("Building packages, total (%s)", pkgsInOrder.length);
-
-  for (const path of pkgsInOrder) {
-    console.log("Building %s", path);
-
-    const coreRet = spawnSync("yarn", ["run", "build"], {
-      cwd: path,
-      stdio: "inherit",
-      shell: true,
-    });
-
-    const name = await getPkgName(path);
-
-    expectSuccess(coreRet, `build ${name} failed`);
-    console.log("%s %s", chalk.bold.green("Done"), name);
-  }
-
-  console.log(
-    "%s All (%s) done!",
-    chalk.bold.green("Success"),
-    pkgsInOrder.length,
-  );
+  await buildInStages(stages);
 }
