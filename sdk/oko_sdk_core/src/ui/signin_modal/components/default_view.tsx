0919e9bdafc2eaf271b0aad0d83d29d2c60e1ae9
chihunmanse <chihunmanse@gmail.com>
2026-02-09 16:46:27 +0900

    sdk_core: update canny link

--- sdk/oko_sdk_core/src/ui/signin_modal/components/default_view.tsx
+++ sdk/oko_sdk_core/src/ui/signin_modal/components/default_view.tsx
@@ -73,7 +73,7 @@ export const DefaultView: FC<DefaultViewProps> = ({
         />
         <a
           className="oko-footer-link"
-          href="https://oko-wallet.canny.io/bug-reports"
+          href="https://okowallet.userjot.com/board/report-bugs"
           target="_blank"
           rel="noopener noreferrer"
         >
