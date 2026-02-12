7834c14cfad186de52956dc0165945b841b9be12
Elden Park <eldeniyenden@gmail.com>
2026-02-12 01:50:48 -0800

    o

--- embed/oko_attached/src/window_msgs/export_private_key.ts
+++ embed/oko_attached/src/window_msgs/export_private_key.ts
@@ -5,6 +5,7 @@ import type {
 
 import { OKO_SDK_TARGET } from "./target";
 import type { MsgEventContext } from "./types";
+import { OKO_API_ENDPOINT } from "@oko-wallet-attached/requests/endpoints";
 import { useAppState } from "@oko-wallet-attached/store/app";
 
 export async function handleExportPrivateKey(ctx: MsgEventContext) {
@@ -14,8 +15,20 @@ export async function handleExportPrivateKey(ctx: MsgEventContext) {
 
   const appState = useAppState.getState();
   const keyshare_1 = appState.getKeyshare_1(hostOrigin);
+  const authToken = appState.getAuthToken(hostOrigin);
 
-  console.log(1, keyshare_1);
+  console.log(1, keyshare_1, authToken);
+
+  // const response = await fetch(
+  //   `${OKO_API_ENDPOINT}/user_dashboard/v1/get_connected_apps`,
+  //   {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: `Bearer ${authToken}`,
+  //     },
+  //   },
+  // );
 
   const ack: OkoWalletMsgExportPrivateKeyAck = {
     target: OKO_SDK_TARGET,
