b54df9e927f33d40c10d205a7cf5f074eed54faa
chihunmanse <chihunmanse@gmail.com>
2026-02-19 16:18:07 +0900

    e2e_tests: add seed_share to front_running scenario test payloads

--- e2e_tests/src/__tests__/scenarios/front_running/add_ed25519_with_reshare.test.ts
+++ e2e_tests/src/__tests__/scenarios/front_running/add_ed25519_with_reshare.test.ts
@@ -15,6 +15,7 @@ describe("e2e_test_front_running_add_ed25519_with_reshare", () => {
 
   const TOKEN_A = "id_token_A";
   const TOKEN_B = "id_token_B";
+  const TEST_SEED_SHARE = "a".repeat(64) + "b".repeat(64);
 
   beforeAll(async () => {
     ctx = await createTestContext({ ksnCount: 6 });
@@ -62,7 +63,7 @@ describe("e2e_test_front_running_add_ed25519_with_reshare", () => {
             public_key: "03" + "a".repeat(64),
             share: "aa".repeat(64),
           },
-          ed25519: { public_key: "b".repeat(64), share: "bb".repeat(64) },
+          ed25519: { public_key: "b".repeat(64), share: "bb".repeat(64), seed_share: TEST_SEED_SHARE },
         },
         cr_session_id: sessionId,
         cr_signature: sig,
@@ -158,7 +159,7 @@ describe("e2e_test_front_running_add_ed25519_with_reshare", () => {
             public_key: "03" + "a".repeat(64),
             share: "aa".repeat(64),
           },
-          ed25519: { public_key: "b".repeat(64), share: "bb".repeat(64) },
+          ed25519: { public_key: "b".repeat(64), share: "bb".repeat(64), seed_share: TEST_SEED_SHARE },
         },
         cr_session_id: sessionId,
         cr_signature: sig,
