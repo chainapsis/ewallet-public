4bc83e5b6418b730a880c8b68472003a14a4402d
lidarbtc <lidarbtc@protonmail.com>
2026-03-04 20:07:49 +0900

    o

--- embed/oko_attached/src/components/modal_variants/eth/arbitrary_sig/ethereum_arbitrary_signature_content.tsx
+++ embed/oko_attached/src/components/modal_variants/eth/arbitrary_sig/ethereum_arbitrary_signature_content.tsx
@@ -1,12 +1,12 @@
-import { type FC } from "react";
+import { useState, type FC } from "react";
 import type { EthereumArbitrarySignPayload } from "@oko-wallet/oko-sdk-core";
 import { Spacing } from "@oko-wallet/oko-common-ui/spacing";
 import { Typography } from "@oko-wallet/oko-common-ui/typography";
+import { ChevronRightIcon } from "@oko-wallet/oko-common-ui/icons/chevron_right";
 import { bytesToString, hexToString } from "viem";
 
 import { MetadataContent } from "@oko-wallet-attached/components/modal_variants/common/metadata_content/metadata_content";
 import { MakeSignatureRawCodeBlock } from "@oko-wallet-attached/components/modal_variants/common/make_signature/make_sig_modal_code_block";
-import { MakeSignatureRawCodeBlockContainer } from "@oko-wallet-attached/components/modal_variants/common/make_signature/make_sig_modal_code_block_container";
 import styles from "./ethereum_arbitrary_signature_content.module.scss";
 
 interface EthereumArbitrarySignatureContentProps {
@@ -16,6 +16,8 @@ interface EthereumArbitrarySignatureContentProps {
 export const EthereumArbitrarySignatureContent: FC<
   EthereumArbitrarySignatureContentProps
 > = ({ payload }) => {
+  const [isViewRawData, setIsViewRawData] = useState(false);
+
   const message = (() => {
     const message = payload.data.message;
     if (typeof message === "string") {
@@ -42,16 +44,38 @@ export const EthereumArbitrarySignatureContent: FC<
         signer={payload.signer}
       />
       <Spacing height={28} />
-      <Typography color="tertiary" size="sm" weight="semibold">
-        Message
-      </Typography>
+      <div className={styles.messageHeader}>
+        <Typography size="sm" color="secondary" weight="semibold">
+          Message
+        </Typography>
+        <div
+          onClick={() => setIsViewRawData(!isViewRawData)}
+          className={styles.viewButtonTextRow}
+        >
+          <Typography size="xs" color="tertiary" weight="medium">
+            {isViewRawData ? "Smart View" : "Raw View"}
+          </Typography>
+          <ChevronRightIcon color="var(--fg-tertiary)" />
+        </div>
+      </div>
       <Spacing height={8} />
-      <MakeSignatureRawCodeBlockContainer>
-        <MakeSignatureRawCodeBlock
-          code={message}
-          className={styles.noMinHeight}
-        />
-      </MakeSignatureRawCodeBlockContainer>
+      <div className={styles.dataContainer}>
+        {isViewRawData ? (
+          <MakeSignatureRawCodeBlock
+            code={message}
+            className={styles.noMinHeight}
+          />
+        ) : (
+          <Typography
+            size="sm"
+            color="tertiary"
+            weight="medium"
+            className={styles.data}
+          >
+            {message}
+          </Typography>
+        )}
+      </div>
     </div>
   );
 };
