8642ed385475a34d23800d4721809bcb664b6690
chihunmanse <chihunmanse@gmail.com>
2026-03-04 14:18:48 +0900

    user_dashboard: align gnb account area with figma spec

--- apps/user_dashboard/src/components/dashboard_header/dashboard_header.tsx
+++ apps/user_dashboard/src/components/dashboard_header/dashboard_header.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { AnchoredMenu } from "@oko-wallet/oko-common-ui/anchored_menu";
+import { IconButton } from "@oko-wallet/oko-common-ui/icon_button";
 import { DiscordIcon } from "@oko-wallet/oko-common-ui/icons/discord_icon";
+import { DotsHorizontalIcon } from "@oko-wallet/oko-common-ui/icons/dots_horizontal";
 import { ExternalLinkOutlinedIcon } from "@oko-wallet/oko-common-ui/icons/external_link_outlined";
 import { GoogleIcon } from "@oko-wallet/oko-common-ui/icons/google_icon";
 import { LogoutIcon } from "@oko-wallet/oko-common-ui/icons/logout";
 import { MenuIcon } from "@oko-wallet/oko-common-ui/icons/menu";
 import { TelegramIcon } from "@oko-wallet/oko-common-ui/icons/telegram_icon";
-import { ThreeDotsVerticalIcon } from "@oko-wallet/oko-common-ui/icons/three_dots_vertical";
 import { XCloseIcon } from "@oko-wallet/oko-common-ui/icons/x_close";
 import { XIcon } from "@oko-wallet/oko-common-ui/icons/x_icon";
 import { Typography } from "@oko-wallet/oko-common-ui/typography";
@@ -130,28 +131,30 @@ export const DashboardHeader: FC<{
 
       <div className={styles.rightSection}>
         {isSignedIn && (
-          <AnchoredMenu
-            placement="bottom-end"
-            TriggerComponent={
-              <div className={styles.accountTrigger}>
-                {authType !== "auth0" && (
-                  <span className={styles.authProviderIcon}>
-                    {getAuthProviderIcon(authType, 24)}
-                  </span>
-                )}
-                <Typography
+          <div className={styles.accountArea}>
+            {authType !== "auth0" && (
+              <span className={styles.authProviderIcon}>
+                {getAuthProviderIcon(authType, 24)}
+              </span>
+            )}
+            <Typography
+              size="md"
+              weight="medium"
+              color="secondary"
+              className={styles.accountEmail}
+            >
+              {displayIdentifier}
+            </Typography>
+            <AnchoredMenu
+              placement="bottom-end"
+              TriggerComponent={
+                <IconButton
+                  hierarchy="tertiary"
                   size="sm"
-                  color="secondary"
-                  className={styles.accountEmail}
-                >
-                  {displayIdentifier}
-                </Typography>
-                <span className={styles.dotsButton}>
-                  <ThreeDotsVerticalIcon color="var(--fg-quaternary)" />
-                </span>
-              </div>
-            }
-            HeaderComponent={
+                  icon={<DotsHorizontalIcon size={20} />}
+                />
+              }
+              HeaderComponent={
               <div className={styles.menuHeader}>
                 <div className={styles.menuUserInfo}>
                   {authType !== "auth0" && (
@@ -229,7 +232,8 @@ export const DashboardHeader: FC<{
               ],
             }}
             className={styles.accountMenu}
-          />
+            />
+          </div>
         )}
 
         <span className={styles.menuIconWrapper} onClick={toggleLeftBarOpen}>
