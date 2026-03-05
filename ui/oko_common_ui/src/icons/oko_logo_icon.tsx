ef1a19d4404b180ecae7862d04268d8f8c98a20a
lidarbtc <lidarbtc@protonmail.com>
2026-03-05 18:13:03 +0900

    oko_common_ui: add theme support to logo

--- ui/oko_common_ui/src/icons/oko_logo_icon.tsx
+++ ui/oko_common_ui/src/icons/oko_logo_icon.tsx
@@ -7,10 +7,17 @@ export const OkoLogoIcon: FC<OkoLogoIconProps> = ({
   width = 72,
   height = 28,
   className,
+  theme,
 }) => {
+  const _theme = theme ?? "dark";
+
   return (
     <img
-      src={`${s3BucketURL}/oko_logo.png`}
+      src={
+        _theme === "light"
+          ? `${s3BucketURL}/oko_logo.png`
+          : `${s3BucketURL}/oko_logo_dark_v2.png`
+      }
       alt="oko_logo_icon"
       width={width}
       height={height}
