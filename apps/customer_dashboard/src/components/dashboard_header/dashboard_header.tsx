07ec4cb393c6a9b3e65d26e688140a72397b84f6
chihunmanse <chihunmanse@gmail.com>
2026-03-06 18:30:50 +0900

    o

--- apps/customer_dashboard/src/components/dashboard_header/dashboard_header.tsx
+++ apps/customer_dashboard/src/components/dashboard_header/dashboard_header.tsx
@@ -1,13 +1,16 @@
 import { Logo } from "@oko-wallet/oko-common-ui/logo";
+import Link from "next/link";
 import type { FC } from "react";
 
 import styles from "./dashboard_header.module.scss";
+import { paths } from "@oko-wallet-ct-dashboard/paths";
 
 export const DashboardHeader: FC = () => {
   return (
     <div className={styles.wrapper}>
-      {/* NOTE: theme is hardcoded to light for now */}
-      <Logo />
+      <Link href={paths.home}>
+        <Logo theme="light" />
+      </Link>
     </div>
   );
 };
