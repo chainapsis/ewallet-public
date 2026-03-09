d29d877ad4c2188eb14a06065af6437fe0cd97b6
chihunmanse <chihunmanse@gmail.com>
2026-03-09 17:23:35 +0900

    oko_common_ui: fix badge font weight to medium

--- ui/oko_common_ui/src/badge/badge.tsx
+++ ui/oko_common_ui/src/badge/badge.tsx
@@ -1,7 +1,7 @@
 import cn from "classnames";
 
-import { Typography } from "@oko-wallet-common-ui/typography/typography";
 import styles from "./badge.module.scss";
+import { Typography } from "@oko-wallet-common-ui/typography/typography";
 
 type Color = "gray" | "success" | "warning" | "error" | "brand";
 type Size = "sm" | "md" | "lg";
@@ -40,7 +40,7 @@ export const Badge = ({
       <Typography
         tagType="span"
         size={getSize(size)}
-        weight="regular"
+        weight="medium"
         customColor={`${color}-700`}
       >
         {label}
