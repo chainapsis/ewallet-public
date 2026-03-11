355cd627fdaa57b29882aef00f8704aa043dbd5e
chihunmanse <chihunmanse@gmail.com>
2026-03-11 12:12:40 +0900

    ct_dashboard: reduce client timer to 59s for additional expiry buffer

--- apps/customer_dashboard/src/constants/index.ts
+++ apps/customer_dashboard/src/constants/index.ts
@@ -5,6 +5,6 @@ export const PASSWORD_CONTAINS_NUMBER_REGEX = /\d/;
 
 export const SIX_DIGITS_REGEX = /^\d{6}$/;
 
-export const EMAIL_VERIFICATION_TIMER_SECONDS = 60;
+export const EMAIL_VERIFICATION_TIMER_SECONDS = 59;
 
 export const GET_STARTED_URL = "https://form.typeform.com/to/MxrBGq9b";
