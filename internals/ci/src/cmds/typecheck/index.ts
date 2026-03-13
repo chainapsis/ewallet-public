9228de0b28130682067bed7ee0652bacb63e7794
chihunmanse <chihunmanse@gmail.com>
2026-03-13 19:17:29 +0900

    ci: add attached_proxy_web to typecheck

--- internals/ci/src/cmds/typecheck/index.ts
+++ internals/ci/src/cmds/typecheck/index.ts
@@ -26,6 +26,7 @@ export async function typeCheck(..._args: any[]) {
     paths.oko_admin_web,
     paths.ct_dashboard_web,
     paths.user_dashboard,
+    paths.attached_proxy_web,
   ];
 
   // NOTE: Currently not used
