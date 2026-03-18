5a377929cd776b583f3cd07818d17c04ba017d37
Ryz0nd <ryzond6728@gmail.com>
2026-03-18 19:13:42 +0900

    user_dashboard, oko_attached: fix export key UI per QA review

--- apps/user_dashboard/src/app/export_private_key/page.tsx
+++ apps/user_dashboard/src/app/export_private_key/page.tsx
@@ -126,8 +126,8 @@ const KeyIcon = () => {
 const SectionKeyIcon = () => {
   return (
     <svg
-      width={20}
-      height={20}
+      width={24}
+      height={24}
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
