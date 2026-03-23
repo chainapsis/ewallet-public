46babfb831af03edf1ba07369cc20af47386c508
devmosis <ryzond6728@gmail.com>
2026-03-23 09:55:36 +0900

    demo_web: pass theme to first SDK init to enable pre-paint theme

--- apps/demo_web/src/state/sdk.ts
+++ apps/demo_web/src/state/sdk.ts
@@ -115,6 +115,7 @@ export const useSDKState = create(
         api_key:
           "72bd2afd04374f86d563a40b814b7098e5ad6c7f52d3b8f84ab0c3d05f73ac6c",
         sdk_endpoint: process.env.NEXT_PUBLIC_OKO_SDK_ENDPOINT,
+        theme: useThemeState.getState().theme,
       });
 
       if (initRes.success) {
