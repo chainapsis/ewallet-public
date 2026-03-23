2a3da37075aac775d9ec1a555d84d6ccec2a6fef
chihunmanse <chihunmanse@gmail.com>
2026-03-23 16:57:43 +0900

    customer_dashboard: add invalid invite link page

--- apps/customer_dashboard/src/paths.ts
+++ apps/customer_dashboard/src/paths.ts
@@ -6,4 +6,6 @@ export const paths = {
   reset_password: "/users/reset_password",
   edit_info: "/edit-info",
   team: "/team",
+  team_invite: "/team/invite",
+  team_invite_invalid: "/team/invite/invalid",
 };
