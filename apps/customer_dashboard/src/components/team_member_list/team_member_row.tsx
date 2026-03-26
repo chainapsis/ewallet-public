f115f7d151b408596f4ad8e8d780759f070dc59b
chihunmanse <chihunmanse@gmail.com>
2026-03-26 10:19:00 +0900

    ct_dashboard: hide edit role button for current user on team page

--- apps/customer_dashboard/src/components/team_member_list/team_member_row.tsx
+++ apps/customer_dashboard/src/components/team_member_list/team_member_row.tsx
@@ -80,34 +80,19 @@ const MemberActions: FC<{
 }> = ({ member, onLeave, onEditRole, onRemove, onResend, onCancelInvite }) => {
   if (member.is_current_user) {
     return (
-      <>
-        <Tooltip
-          title="Edit role"
-          placement="top"
-          hideFloatingArrow
-          className={styles.tooltipWrapper}
-        >
-          <IconButton
-            hierarchy="tertiary"
-            size="xs"
-            icon={<UserEditIcon />}
-            onClick={onEditRole}
-          />
-        </Tooltip>
-        <Tooltip
-          title="Leave Team"
-          placement="top"
-          hideFloatingArrow
-          className={styles.tooltipWrapper}
-        >
-          <IconButton
-            hierarchy="tertiary"
-            size="xs"
-            icon={<DoorOutlinedIcon />}
-            onClick={onLeave}
-          />
-        </Tooltip>
-      </>
+      <Tooltip
+        title="Leave Team"
+        placement="top"
+        hideFloatingArrow
+        className={styles.tooltipWrapper}
+      >
+        <IconButton
+          hierarchy="tertiary"
+          size="xs"
+          icon={<DoorOutlinedIcon />}
+          onClick={onLeave}
+        />
+      </Tooltip>
     );
   }
 
